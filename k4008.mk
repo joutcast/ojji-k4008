3c36573aa78cfc0d0d527a6055357c6ba7cdb923
Christopher Lock <joutcast@gmail.com>
2018-06-01 16:55:16 -0700

    Create k4008.mk

---
+++ k4008.mk
@@ -0,0 +1,6 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+PRODUCT_DEVICE := k4008
+PRODUCT_NAME := k4008
+PRODUCT_BRAND := ojji
+PRODUCT_MODEL := MAXX,NITRO
+PRODUCT_MANUFACTURER := ojji

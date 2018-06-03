300440c8639c1dbaa6897353f0fc86c32aa978b8
Christopher Lock <joutcast@gmail.com>
2018-06-03 09:03:22 -0700

    Update Android.mk

--- device/ojji/k4008/Android.mk
+++ device/ojji/k4008/Android.mk
@@ -1,7 +1,7 @@
 # Android
-ifneq ($(filter k4008,$(TARGET_DEVICE)),)
+ifneq ($(filter tc4_k4008,$(TARGET_DEVICE)),)
 
-LOCAL_PATH := device/ojji/k4008/
+LOCAL_PATH := device/ojji/tc4_k4008/
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
